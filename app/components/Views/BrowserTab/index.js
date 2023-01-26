534acf24505c730c800a3ff9ddf161544ca12bf2
Mark Stacey <markjstacey@gmail.com>
2023-01-26 14:26:58 -0330

    Refactor background bridge connection options
    The options passed into the background bridge have been refactored to
    distinguish between the different connection types more effectively.
    
    Previously most constructor options were connection-specific, which
    made this module difficult to read. Now all connection-specific
    information has been grouped together and documented.
    
    A few unused parameters were dropped, but this should include zero
    functional changes.

--- app/components/Views/BrowserTab/index.js
+++ app/components/Views/BrowserTab/index.js
@@ -68,6 +68,7 @@ import { useTheme } from '../../../util/theme';
 import downloadFile from '../../../util/browser/downloadFile';
 import { createBrowserUrlModalNavDetails } from '../BrowserUrlModal/BrowserUrlModal';
 import {
+  CONNECTION_TYPE,
   MM_PHISH_DETECT_URL,
   MM_BLOCKLIST_ISSUE_URL,
   PHISHFORT_BLOCKLIST_ISSUE_URL,
@@ -925,8 +926,12 @@ export const BrowserTab = (props) => {
 
   const initializeBackgroundBridge = (urlBridge, isMainFrame) => {
     const newBridge = new BackgroundBridge({
-      webview: webviewRef,
-      url: urlBridge,
+      connection: {
+        isMainFrame,
+        type: CONNECTION_TYPE.BROWSER,
+        url: urlBridge,
+        webview: webviewRef,
+      },
       getRpcMethodMiddleware: ({ hostname, getProviderState }) =>
         getRpcMethodMiddleware({
           hostname,
@@ -946,7 +951,6 @@ export const BrowserTab = (props) => {
           tabId: props.id,
           injectHomePageScripts,
         }),
-      isMainFrame,
     });
     backgroundBridges.current.push(newBridge);
   };
