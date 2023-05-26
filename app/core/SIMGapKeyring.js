0b04042781d38e438c8f88bf9b2f39cbc194682d
PeiHuang <peichunhuang1133@gmail.com>
2023-05-26 15:14:33 +0800

    [style] autocomplete

--- app/core/SIMGapKeyring.js
+++ app/core/SIMGapKeyring.js
@@ -1,19 +1,28 @@
 import { EventEmitter } from 'events';
 import { ObservableStore } from '@metamask/obs-store';
-import { CryptoHDKey, CryptoAccount, ETHSignature, EthSignRequest, DataType } from '@keystonehq/bc-ur-registry-eth';
+import {
+  CryptoHDKey,
+  CryptoAccount,
+  ETHSignature,
+  EthSignRequest,
+  DataType,
+} from '@keystonehq/bc-ur-registry-eth';
 import { stringify, v4 } from 'uuid';
 import { TransactionFactory } from '@ethereumjs/tx';
-import { publicToAddress, toChecksumAddress, BN, stripHexPrefix } from 'ethereumjs-util';
+import {
+  publicToAddress,
+  toChecksumAddress,
+  BN,
+  stripHexPrefix,
+} from 'ethereumjs-util';
 import { Transaction } from '@ethereumjs/tx';
 import SIMGapWallet from './SIMGapWallet';
 import Logger from '../util/Logger';
-import {PermissionsAndroid, DeviceEventEmitter, Modal} from 'react-native';
+import { PermissionsAndroid, DeviceEventEmitter, Modal } from 'react-native';
 
-
-const keyringType = "SIMGap Wallet Device";
+const keyringType = 'SIMGap Wallet Device';
 
 var _initListeners = false;
-
 
 class SIMGapKeyring {
   constructor(opts) {
@@ -30,13 +39,12 @@ class SIMGapKeyring {
     this.accounts = [];
     this.currentAccount = 0;
     this.unlockedAccount = 0;
-    this.name = "SIMGap Hardware";
+    this.name = 'SIMGap Hardware';
     this.initialized = false; //hd props;
 
     this.deserialize(opts);
 
-
-    this.setAccountToUnlock = index => {
+    this.setAccountToUnlock = (index) => {
       this.unlockedAccount = parseInt(index, 10);
     };
 
@@ -46,123 +54,115 @@ class SIMGapKeyring {
     }
 
     SIMGapKeyring.instance = this;
-    DeviceEventEmitter.addListener('EVENT_SIMGAP_LOG', (msg)=>{
-      Logger.log("SIMGapWallet Log: " + msg);
+    DeviceEventEmitter.addListener('EVENT_SIMGAP_LOG', (msg) => {
+      Logger.log('SIMGapWallet Log: ' + msg);
     });
-    DeviceEventEmitter.addListener('EVENT_SIMGAP_ERROR', (msg)=>{
-      Logger.log("SIMGapWallet Error: " + msg);
+    DeviceEventEmitter.addListener('EVENT_SIMGAP_ERROR', (msg) => {
+      Logger.log('SIMGapWallet Error: ' + msg);
     });
   }
 
-// exportAccount should return a hex-encoded private key:
+  // exportAccount should return a hex-encoded private key:
   exportAccount(address, opts = {}) {
     throw new Error(`Private key export is not supported.`);
   }
 
-// removeAccount
+  // removeAccount
   removeAccount(address) {
-    if (!this.accounts.map(a => a.toLowerCase()).includes(address.toLowerCase())) {
+    if (
+      !this.accounts.map((a) => a.toLowerCase()).includes(address.toLowerCase())
+    ) {
       throw new Error(`Address ${address} not found in this keyring`);
     }
 
-    this.accounts = this.accounts.filter(a => a.toLowerCase() !== address.toLowerCase());
+    this.accounts = this.accounts.filter(
+      (a) => a.toLowerCase() !== address.toLowerCase(),
+    );
   }
 
-// addAccounts
+  // addAccounts
   async addAccounts(n = 1) {
     const from = this.unlockedAccount;
     const to = from + n;
     const newAccounts = [];
 
     for (let i = from; i < to; i++) {
-//      const address = await OTIColdWallet.newWallet();
+      //      const address = await OTIColdWallet.newWallet();
       const address = this.accs[i].address;
       newAccounts.push(address);
       this.page = 0;
       this.unlockedAccount++;
     }
 
-
-
-Logger.log("=== Add " + newAccounts + " accounts to " + this.accounts);
+    Logger.log('=== Add ' + newAccounts + ' accounts to ' + this.accounts);
     this.accounts = this.accounts.concat(newAccounts);
-Logger.log("=== Now accounts is " + this.accounts);
+    Logger.log('=== Now accounts is ' + this.accounts);
     return this.accounts;
   }
 
-
-// getAccounts
+  // getAccounts
   getAccounts() {
     return Promise.resolve(this.accounts);
   }
 
-// signTransaction
+  // signTransaction
   // tx is an instance of the ethereumjs-transaction class.
   async signTransaction(address, tx) {
-    const {
-      r,
-      s,
-      v
-    } = await SIMGapWallet.signTransaction(address, tx.type, tx.getMessageToSign(false), tx.common.chainId());
+    const { r, s, v } = await SIMGapWallet.signTransaction(
+      address,
+      tx.type,
+      tx.getMessageToSign(false),
+      tx.common.chainId(),
+    );
     const txJson = tx.toJSON();
     txJson.v = v;
     txJson.s = s;
     txJson.r = r;
     txJson.type = tx.type;
     const transaction = TransactionFactory.fromTxData(txJson, {
-      common: tx.common
+      common: tx.common,
     });
     return transaction;
   }
 
-// signMessage
+  // signMessage
   async signMessage(withAccount, data) {
-    const {
-      r,
-      s,
-      v
-    } = await SIMGapWallet.signMessage(withAccount, data);
-    return "0x" + Buffer.concat([r, s, v]).toString("hex");
+    const { r, s, v } = await SIMGapWallet.signMessage(withAccount, data);
+    return '0x' + Buffer.concat([r, s, v]).toString('hex');
   }
 
-// signPersonalMessage
+  // signPersonalMessage
   async signPersonalMessage(withAccount, messageHex) {
-    const {
-      r,
-      s,
-      v
-    } = await SIMGapWallet.signPersonalMessage(withAccount, messageHex);
-    return "0x" + Buffer.concat([r, s, v]).toString("hex");
+    const { r, s, v } = await SIMGapWallet.signPersonalMessage(
+      withAccount,
+      messageHex,
+    );
+    return '0x' + Buffer.concat([r, s, v]).toString('hex');
   }
 
-// signTypedData
+  // signTypedData
   async signTypedData(withAccount, typedData) {
-    const {
-      r,
-      s,
-      v
-    } = await SIMGapWallet.signTypedData(withAccount, Buffer.from(JSON.stringify(typedData), "utf-8"));
-    return "0x" + Buffer.concat([r, s, v]).toString("hex");
+    const { r, s, v } = await SIMGapWallet.signTypedData(
+      withAccount,
+      Buffer.from(JSON.stringify(typedData), 'utf-8'),
+    );
+    return '0x' + Buffer.concat([r, s, v]).toString('hex');
   }
 
-// decryptMessage
+  // decryptMessage
   // For eth_decryptMessage:
-  async decryptMessage (withAccount, encryptedData) {
+  async decryptMessage(withAccount, encryptedData) {
     const dec = await SIMGapWallet.signTypedData(withAccount, encryptedData);
     return dec;
   }
 
-// getEncryptionPublicKey
+  // getEncryptionPublicKey
   // get public key for nacl
-  async getEncryptionPublicKey (withAccount) {
-    return  await SIMGapWallet.getEncryptionPublicKey(withAccount);
+  async getEncryptionPublicKey(withAccount) {
+    return await SIMGapWallet.getEncryptionPublicKey(withAccount);
   }
 
-// getAppKeyAddress
-
-
-
-
+  // getAppKeyAddress
 
   serialize() {
     return Promise.resolve({
@@ -191,7 +191,6 @@ Logger.log("=== Now accounts is " + this.accounts);
     }
   }
 
-
   getFirstPage() {
     this.page = 0;
     return this.__getPage(1);
@@ -207,48 +206,49 @@ Logger.log("=== Now accounts is " + this.accounts);
 
   async __getPage(increment) {
     if (!this.initialized) {
-//      await this.readKeyring();
+      //      await this.readKeyring();
 
       this.accs = await SIMGapWallet.fetchResult(1); //SIMGapWallet.available();
-      Logger.log("...............this.accs is " + (this.accs==null?"":"NOT ") + "null.");
-      if (this.accs==null) return [];
-      Logger.log("---------------this.accs is " + this.accs);
+      Logger.log(
+        '...............this.accs is ' +
+          (this.accs == null ? '' : 'NOT ') +
+          'null.',
+      );
+      if (this.accs == null) return [];
+      Logger.log('---------------this.accs is ' + this.accs);
       this.initialized = true;
     }
 
-      this.page += increment;
+    this.page += increment;
 
-      if (this.page <= 0) {
-        this.page = 1;
-      }
+    if (this.page <= 0) {
+      this.page = 1;
+    }
 
-      var i = (this.page - 1) * this.perPage;
-      var to = i + this.perPage;
-      const accounts = [];
+    var i = (this.page - 1) * this.perPage;
+    var to = i + this.perPage;
+    const accounts = [];
 
-      if (i>=this.accs.length) return accounts;
-      if (to>this.accs.length) to = this.accs.length;
+    if (i >= this.accs.length) return accounts;
+    if (to > this.accs.length) to = this.accs.length;
 
-      while (i < to) {
-        const address = this.accs[i].address;
-        accounts.push({
-          address,
-          balance: null,
-          index: i
-        });
-//        this.indexes[toChecksumAddress(address)] = i;
-        i++;
-      }
-      Logger.log('this.page: ' + this.page );
-      Logger.log('accounts: ' + accounts );
-      Logger.log('this.accs: ' + this.accs );
+    while (i < to) {
+      const address = this.accs[i].address;
+      accounts.push({
+        address,
+        balance: null,
+        index: i,
+      });
+      //        this.indexes[toChecksumAddress(address)] = i;
+      i++;
+    }
+    Logger.log('this.page: ' + this.page);
+    Logger.log('accounts: ' + accounts);
+    Logger.log('this.accs: ' + this.accs);
 
-      return accounts;
+    return accounts;
   }
-
 }
-
-
 
 SIMGapKeyring.type = keyringType;
 
--- app/core/SIMGapWallet.js
+++ app/core/SIMGapWallet.js
@@ -17,127 +17,164 @@ class SIMGapWallet {
 export default {
   // { address: string; index: number; balance: string }
   async available() {
-Logger.log('Calling SIMGapWallet.available()...');
-  	if (isAndroid) {
-  	  var rr = await coldWallet.available();
-  	  if (rr=="null")
-  	  {
-Logger.log('SIMGapWallet.available() got a null response.');
-  	    return null;
-  	  }
-  	  return rr;
+    Logger.log('Calling SIMGapWallet.available()...');
+    if (isAndroid) {
+      var rr = await coldWallet.available();
+      if (rr == 'null') {
+        Logger.log('SIMGapWallet.available() got a null response.');
+        return null;
+      }
+      return rr;
     }
   },
   async fetchResult(fid) {
-Logger.log('Calling SIMGapWallet.fetchResult(' + fid + ')...');
-      var rr = await coldWallet.fetchResult(fid);
-  	  if (rr=="null")
-  	  {
-Logger.log('SIMGapWallet.available() got a null response.');
-  	    return null;
-  	  }
+    Logger.log('Calling SIMGapWallet.fetchResult(' + fid + ')...');
+    var rr = await coldWallet.fetchResult(fid);
+    if (rr == 'null') {
+      Logger.log('SIMGapWallet.available() got a null response.');
+      return null;
+    }
 
-      if (fid==1) // available
-      {
-	  	  var accounts = new Array();
-	  	  var i;
-	  	  var res = rr.split("|");
-	Logger.log('    ' + res.length + ' accounts are returned.');
-	  	  for (i=0; i<res.length; i++)
-	      {
-	Logger.log("    Account-" + i);
-	      	var itm = res[i].split("@");
+    if (fid == 1) {
+      // available
+      var accounts = new Array();
+      var i;
+      var res = rr.split('|');
+      Logger.log('    ' + res.length + ' accounts are returned.');
+      for (i = 0; i < res.length; i++) {
+        Logger.log('    Account-' + i);
+        var itm = res[i].split('@');
 
-	      	const addr = itm[0];
-	      	const idx = parseInt(itm[1], 10);
-	      	const blnc = itm[2];
+        const addr = itm[0];
+        const idx = parseInt(itm[1], 10);
+        const blnc = itm[2];
 
-	Logger.log("       addr: " + addr);
-	Logger.log("       idx: " + idx);
-	Logger.log("       blnc: " + blnc);
+        Logger.log('       addr: ' + addr);
+        Logger.log('       idx: ' + idx);
+        Logger.log('       blnc: ' + blnc);
 
-	      	const acc = {address: addr, index:idx, balance: blnc};
-	      	accounts[i] = acc;
-	      }
-	  	  return accounts;
-	  }
-	  else if (fid==2) // signTransaction
-      {
-      	let signature = rr;
-		const r = signature.slice(0, 32);
-		const s = signature.slice(32, 64);
-		const v = signature.slice(64);
-		return {
-		  r,
-		  s,
-		  v
-		};
+        const acc = { address: addr, index: idx, balance: blnc };
+        accounts[i] = acc;
       }
+      return accounts;
+    } else if (fid == 2) {
+      // signTransaction
+      let signature = rr;
+      const r = signature.slice(0, 32);
+      const s = signature.slice(32, 64);
+      const v = signature.slice(64);
+      return {
+        r,
+        s,
+        v,
+      };
+    }
 
     return null;
   },
 
   newWallet() {
-Logger.log('Calling SIMGapWallet.newWallet()...');
+    Logger.log('Calling SIMGapWallet.newWallet()...');
     var rr = coldWallet.newWallet();
-    if (rr=="null") return null;
+    if (rr == 'null') return null;
     return rr;
   },
 
   // transType= 0 Transaction; others: typedTransaction
   signTransaction(address, transType, messageToSign, chainId) {
-Logger.log('Calling SIMGapWallet.signTransaction(address = ' + address + ', transType = ' + transType + ', messageToSign = ' + messageToSign + ', chainId = ' + chainId + ')...');
-  	let signature = coldWallet.signTransaction(address, transType, messageToSign, chainId);
-  	if (signature=="null") return null;
+    Logger.log(
+      'Calling SIMGapWallet.signTransaction(address = ' +
+        address +
+        ', transType = ' +
+        transType +
+        ', messageToSign = ' +
+        messageToSign +
+        ', chainId = ' +
+        chainId +
+        ')...',
+    );
+    let signature = coldWallet.signTransaction(
+      address,
+      transType,
+      messageToSign,
+      chainId,
+    );
+    if (signature == 'null') return null;
   },
 
   signMessage(withAccount, data) {
-Logger.log('Calling SIMGapWallet.signMessage(withAccount = ' + withAccount + ', data = ' + data + ')...');
+    Logger.log(
+      'Calling SIMGapWallet.signMessage(withAccount = ' +
+        withAccount +
+        ', data = ' +
+        data +
+        ')...',
+    );
     let signature = coldWallet.signMessage(withAccount, data);
-  	if (signature=="null") return null;
-  	const r = signature.slice(0, 32);
+    if (signature == 'null') return null;
+    const r = signature.slice(0, 32);
     const s = signature.slice(32, 64);
     const v = signature.slice(64);
     return {
       r,
       s,
-      v
+      v,
     };
   },
 
   signPersonalMessage(withAccount, messageHex) {
-Logger.log('Calling SIMGapWallet.signPersonalMessage(withAccount = ' + withAccount + ', messageHex = ' + messageHex + ')...');
+    Logger.log(
+      'Calling SIMGapWallet.signPersonalMessage(withAccount = ' +
+        withAccount +
+        ', messageHex = ' +
+        messageHex +
+        ')...',
+    );
     let signature = coldWallet.signPersonalMessage(withAccount, messageHex);
-  	if (signature=="null") return null;
-  	const r = signature.slice(0, 32);
+    if (signature == 'null') return null;
+    const r = signature.slice(0, 32);
     const s = signature.slice(32, 64);
     const v = signature.slice(64);
     return {
       r,
       s,
-      v
+      v,
     };
   },
 
   signTypedData(withAccount, typedData) {
-Logger.log('Calling SIMGapWallet.signTypedData(withAccount = ' + withAccount + ', typedData = ' + typedData + ')...');
+    Logger.log(
+      'Calling SIMGapWallet.signTypedData(withAccount = ' +
+        withAccount +
+        ', typedData = ' +
+        typedData +
+        ')...',
+    );
     let rr = coldWallet.signTypedData(withAccount, typedData);
-  	if (rr=="null")
-  	{
-Logger.log('SIMGapWallet.signTypedData() got a null response.');
-  	  return null;
-  	}
-  	return rr;
+    if (rr == 'null') {
+      Logger.log('SIMGapWallet.signTypedData() got a null response.');
+      return null;
+    }
+    return rr;
   },
 
-  decryptMessage (withAccount, encryptedData) {
-Logger.log('Calling SIMGapWallet.decryptMessage(withAccount = ' + withAccount + ', encryptedData = ' + encryptedData + ')...');
-  	return coldWallet.signTypedData(withAccount, encryptedData);
+  decryptMessage(withAccount, encryptedData) {
+    Logger.log(
+      'Calling SIMGapWallet.decryptMessage(withAccount = ' +
+        withAccount +
+        ', encryptedData = ' +
+        encryptedData +
+        ')...',
+    );
+    return coldWallet.signTypedData(withAccount, encryptedData);
   },
 
-  getEncryptionPublicKey (withAccount) {
-Logger.log('Calling SIMGapWallet.getEncryptionPublicKey(withAccount = ' + withAccount + ')...');
-  	return coldWallet.getEncryptionPublicKey(withAccount);
-  }
+  getEncryptionPublicKey(withAccount) {
+    Logger.log(
+      'Calling SIMGapWallet.getEncryptionPublicKey(withAccount = ' +
+        withAccount +
+        ')...',
+    );
+    return coldWallet.getEncryptionPublicKey(withAccount);
+  },
 };
-
